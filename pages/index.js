3ac4208dcd54659f231acf6de035693ad0dc1eee
Onseted <108707011+Onseted@users.noreply.github.com>
2022-07-18 16:48:06 +0900

    "반응3"

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Footer Copywrite autorized from VERCEL
+          Copywrite : VERCEL
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
